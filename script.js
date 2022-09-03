680da48f4ebff69b10908b12366d024526d65d67
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-09-03 01:52:22 -0700

    about me section

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed=new Typed(".typing-2",{
+    strings:["PERFORMANCE FOCUSED","GROWTH-ORIENTED","CRITICAL THINKER", " EFFECTIVE COMMUNICATOR","DECISIVE","CALM UNDER PRESSURE" ,"ROSY-OUTLOOK"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
